7d7a2c004c0c7ba975dfa8e00a17532cef712f5c
eliskol <75465281+eliskol@users.noreply.github.com>
2026-02-17 06:03:55 +0000

    Deploying to gh-pages from @ eliskol/eliskol.github.io@575fd81c3061b0eb4adaf6b6079d06245227c02b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,23 +25,26 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "My CV/resume.",
+          description: "My CV/resume. (still under construction lol)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "courses-math-220b-modern-algebra-ii",
+        },{id: "courses-math-220a-modern-algebra-i",
+          title: 'MATH 220A Modern Algebra I',
+          description: "Graduate course in modern algebra at UCSB.",
+          section: "Courses",handler: () => {
+              window.location.href = "/courses/MATH220A/";
+            },},{id: "courses-math-220b-modern-algebra-ii",
           title: 'MATH 220B Modern Algebra II',
           description: "Graduate course in modern algebra at UCSB.",
           section: "Courses",handler: () => {
               window.location.href = "/courses/MATH220B/";
+            },},{id: "courses-math-cs-120ai",
+          title: 'MATH CS 120AI',
+          description: "CCS special topics course on machine learning/data science.",
+          section: "Courses",handler: () => {
+              window.location.href = "/courses/MATHCS120AI/";
             },},{id: "news-just-set-this-up",
           title: 'Just set this up!',
           description: "",
